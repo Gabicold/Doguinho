19c9d3fd275312883e1b4c03afef70dbb0c75d5c
Gabicold <biel18102004@gmail.com>
2022-06-13 20:39:31 -0300

    doguinho fofo 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import styled from "styled-components"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const CaixaPrincipal = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+  width:100vw;
+  height:100vh;
+  background: #9400d3;
+  `
+  const Titulo = styled.h1`
+  color:White;
+  `
+  const Rodrigo = styled.button`
+  color:white;
+  font-size:1.2rem;
+  font-weight:bold;
+  background-color: #229a00;
+  border:solid 2px #229a00;
+  width:15vw;
+  border-radius:20px;
+  height:5vh;
+  cursor:pointer;
+  &:hover{
+    border-radius:10px;
+    transition-duration: 1s;
+    
+  }
+  `
+
+  const Doguinhos = styled.img`
+  whidth:5vw;
+  height:40vh;
+  margin-top:5vh;
+  border-radius:40px 40px 1px;
+  `
+  const [dog, setDog] = useState([])
+  const [appear, setAppear] = useState(false)
+  const DogList = () => {
+    axios.get(`https://dog.ceo/api/breeds/image/random`).then((response) => {
+      setDog(response.data.message);
+    });
+    setAppear(true)
+  }
+
+ 
+  return(
+<CaixaPrincipal>
+    <Titulo>Clique para soltar vários "KKKS"</Titulo>
+    <Rodrigo onClick={()=>{DogList()}}>Alegria</Rodrigo>
+    <div>
+    {appear && <Doguinhos src={dog} alt="doguinhos"/>}
     </div>
-  );
+    
+</CaixaPrincipal>
+  )
 }
 
-export default App;
